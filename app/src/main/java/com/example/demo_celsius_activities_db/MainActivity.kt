aa813d0fa9d792d03135aac0f395f56a741e766d
LuisGTP12 <monterrosaluis@gmail.com>
2025-05-26 21:55:45 -0500

    commit--emote

--- app/src/main/java/com/example/demo_celsius_activities_db/MainActivity.kt
+++ app/src/main/java/com/example/demo_celsius_activities_db/MainActivity.kt
@@ -3,77 +3,64 @@ package com.example.demo_celsius_activities_db
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 
-/**
- * Justin Barrios, Cédula: 8-983-1021
- * Luis Monterrosa, Cédula: 8-1014-1095
- * Aaron Santamaria, Cédula: 3-742-1763
- */
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(), View.OnClickListener  {
-    lateinit var boton1 : Button
-    lateinit var boton2 : Button
-    lateinit var boton3 : Button
+    private lateinit var boton1: RadioButton
+    private lateinit var boton2: RadioButton
+    private lateinit var boton3: RadioButton
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        boton1 = findViewById(R.id.button1)
-        boton1.setOnClickListener(this)
+        // Inicializar vistas
+        boton1 = findViewById(R.id.radioButton)
+        boton2 = findViewById(R.id.radioButton2)
+        boton3 = findViewById(R.id.radioButton3)
+        radioGroup = findViewById(R.id.radioGroup)
+        editText = findViewById(R.id.editText1)
 
-        boton2 = findViewById(R.id.button2)
-        boton2.setOnClickListener(this)
+        // Establecer listener
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val celsius = editText.text.toString().toDoubleOrNull()
 
-        boton3 = findViewById(R.id.button3)
-        boton3.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.button1 -> {
-                val celsius = findViewById<EditText>(R.id.editText1)
-                val resultado = celsius_fahrenheit(celsius.text.toString().toDouble())
-
-                val intent = Intent(this, SecondActivity::class.java)
-                intent.putExtra("RESULTADO", resultado)
-                startActivity(intent)
+            if (!(celsius == null)) {
+            val resultado = when (checkedId) {
+                R.id.radioButton -> celsius_fahrenheit(celsius)
+                R.id.radioButton2 -> celsius_kelvin(celsius)
+                R.id.radioButton3 -> celsius_rankine(celsius)
+                else -> return@setOnCheckedChangeListener
             }
-            R.id.button2 -> {
-                val celsius = findViewById<EditText>(R.id.editText1)
-                val resultado = celsius_kelvin(celsius.text.toString().toDouble())
 
-                val intent = Intent(this, SecondActivity::class.java)
-                intent.putExtra("RESULTADO", resultado)
-                startActivity(intent)
-            }
-            R.id.button3 -> {
-                val celsius = findViewById<EditText>(R.id.editText1)
-                val resultado = celsius_rankine(celsius.text.toString().toDouble())
-
-                val intent = Intent(this, SecondActivity::class.java)
-                intent.putExtra("RESULTADO", resultado)
-                startActivity(intent)
-            }
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("RESULTADO", resultado)
+            startActivity(intent)
+        }else {
+                Toast.makeText( this,"ingrese un numero para convertir", Toast.LENGTH_LONG).show()
+        }
         }
     }
 
-    private fun celsius_fahrenheit(celsius:Double): Double {
-        val fahrenheit = (celsius * 9/5)+32
-        return fahrenheit
+    private fun celsius_fahrenheit(celsius: Double): Double {
+        val conver=(celsius * 9 / 5) + 32
+        return (conver * 100).toInt() / 100.0
     }
 
-    private fun celsius_kelvin(celsius:Double): Double {
-        val kelvin = celsius + 273.75
-        return kelvin
+    private fun celsius_kelvin(celsius: Double): Double {
+        val conver = celsius + 273.15
+        return (conver * 100).toInt() / 100.0
     }
 
-    private fun celsius_rankine(celsius:Double): Double {
-        val rankine = (celsius * 9/5)+491.67
-        return rankine
-    }
+    private fun celsius_rankine(celsius: Double): Double {
+        val conver=(celsius * 9 / 5) + 491.67
+        return  (conver * 100).toInt() / 100.0
 
+    }
 }
